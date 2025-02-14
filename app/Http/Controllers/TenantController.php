5c34787b2e04219c245f0fa6e1c5efbc15748471
Anisur Rahman <anisurlist3@gmail.com>
2025-02-14 14:50:02 +0600

    Organize and refactor code

--- app/Http/Controllers/TenantController.php
+++ app/Http/Controllers/TenantController.php
@@ -13,7 +13,7 @@ class TenantController extends Controller
      */
     public function index()
     {
-        return inertia('Tenant/Index', [
+        return inertia('Admin/Index', [
             'tenants' => Tenant::with('domains')->get(),
         ]);
     }
@@ -23,7 +23,7 @@ class TenantController extends Controller
      */
     public function create()
     {
-        return inertia('Tenant/Create');
+        return inertia('Admin/Create');
     }
 
     /**
--- routes/tenant.php
+++ routes/tenant.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Tenant\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
@@ -18,12 +21,31 @@ use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 |
 */
 
+
 Route::middleware([
     'web',
     InitializeTenancyByDomain::class,
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
     Route::get('/', function () {
-        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+        return Inertia::render('Welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
     });
+
+    Route::get('/dashboard', function () {
+        return Inertia::render('Tenant/Dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    });
+
+    require __DIR__.'/tenant-auth.php';
 });
